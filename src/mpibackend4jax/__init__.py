ba75fe4bb049194dcc4701029747d8f12f132651
Clemens Giuliani <clemens@inailuig.it>
2025-07-30 15:00:47 +0200

    only set variables if not set previously This way its easier to override by hand

--- src/mpibackend4jax/__init__.py
+++ src/mpibackend4jax/__init__.py
@@ -20,11 +20,12 @@ def initialize():
 
     # Set environment variables for MPITrampoline
     if _mpiwrapper_lib.exists():
-        os.environ["MPITRAMPOLINE_LIB"] = str(_mpiwrapper_lib.absolute())
-        os.environ["JAX_CPU_COLLECTIVES_IMPLEMENTATION"] = "mpi"
-
-        print(f"mpibackend4jax: Set MPITRAMPOLINE_LIB={_mpiwrapper_lib.absolute()}")
-        print("mpibackend4jax: Set JAX_CPU_COLLECTIVES_IMPLEMENTATION=mpi")
+        if "MPITRAMPOLINE_LIB" not in os.environ.keys():
+            os.environ["MPITRAMPOLINE_LIB"] = str(_mpiwrapper_lib.absolute())
+            print(f"mpibackend4jax: Set MPITRAMPOLINE_LIB={_mpiwrapper_lib.absolute()}")
+        if "JAX_CPU_COLLECTIVES_IMPLEMENTATION" not in os.environ.keys()
+            os.environ["JAX_CPU_COLLECTIVES_IMPLEMENTATION"] = "mpi"
+            print("mpibackend4jax: Set JAX_CPU_COLLECTIVES_IMPLEMENTATION=mpi")
     else:
         print(f"Warning: MPIWrapper library not found at {_mpiwrapper_lib}")
         print("Please ensure the package was installed correctly.")
